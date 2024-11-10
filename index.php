c8f80b348dbe4d7c16c28d83eeeef6a1d92f0276
Ehrin- John <ehrinjohn08@gmail.com>
2024-11-10 13:03:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
+
 ?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EmpowerEd</title>
+</head>
+
+<body>
+    <form action="">
+
+    </form>
+</body>
+
+</html>
